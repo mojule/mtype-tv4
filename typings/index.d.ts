f90c97752e7abc2f3f05a08661249b7790e4709e
Andrew Bell <andy.bell@informationage.co.nz>
2017-02-17 14:55:35 +1300

    Changes to typing definitions for Validator function with property

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,8 +1,7 @@
-/// <references path="./tv4.d.ts"/>
 // Type definitions for mtype backed by json schemar mtype-node 0.1.x
 // Project: https://github.com/mojule/mtype-tv4
 
-declare namespace mtypeTv4 {
+declare namespace mTV4 {
 
   /**
    * Object with properties referencing JsonSchema.  Passed to Validator constructor
@@ -12,27 +11,33 @@ declare namespace mtypeTv4 {
     [key: string]: tv4.JsonSchema
   }
 
-
+  /**
+   *Validator interface describes javascript function with attached property mtype
+   *Typescript implementation would be:
+   *   const Validator = <IValidator> function (schemas: JsonSchema): string  { .... }
+   *   Validator.mtype = ( validator ) => {...}
+   *For explanation of Typescript construct refer to http://stackoverflow.com/questions/12766528/build-a-function-object-with-properties-in-typescript
+   */
   export interface Validator {
     /**
-     *Creates a TV4 instance, populates with schemas and returns TV4 api.
-     *Param: Object with named properties referencing JsonSchema.
-     *Returns: Reference to instance of TV4 api populated with passed schemas.
-     */
-    Validator: (schemas: JsonSchemas) => tv4.TV4
-
+  *Creates a TV4 instance, populates with schemas and returns TV4 api.
+  *Param: Object with named properties referencing JsonSchema.
+  *Returns: Reference to instance of TV4 api populated with passed schemas.
+  */
+    (schemas: JsonSchemas): tv4.TV4
 
     /**
      *Static method on Validator wrapping TV4 schema validator in mtype.
      *Param: Instance of Tv4 api (returned by Validator constructor).
      *Returns: Instance of mtype api for validation of json objects against named schema.
      */
-    mtype(validator: tv4.TV4): mtype.api
+    mtype: (validator: tv4.TV4) => mtype.api
   }
 
 }
 
 
-declare var validator : mtypeTv4.Validator
+declare const validator: Validator
+
 export = validator
-export as namespace mtypeTv4
+export as namespace mTV4
